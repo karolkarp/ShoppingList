266b7514fecc53856388ac31553e93787438eb79
karolkarp <karol0karp@gmail.com>
2019-08-07 01:56:58 +0200

    Add element list adding

--- store/actions/purchases.tsx
+++ store/actions/purchases.tsx
@@ -1,1 +1,27 @@
+export function addNewList(addNew: boolean): object{
+	return{
+		type:'ADD_NEW',
+		addNew
+	};
+}
 
+export function setTemporaryList(element:string): object{
+	return{
+		type:'SET_TEMPORARY_LIST',
+		element
+	};
+}
+
+export function setShopLists(shopList: object[]): object{
+	return{
+		type:'SET_LIST',
+		shopList
+	};
+}
+
+export function setShopArchivedLists(shopArchivedLists: object[]): object{
+	return{
+		type:'SET_ARCHIVED_LIST',
+		shopArchivedLists
+	};
+}
--- store/reducers/purchases.tsx
+++ store/reducers/purchases.tsx
@@ -1,17 +1,32 @@
 /* eslint-disable indent */
 interface Actions {
 	type: string;
+	addNew: boolean;
 	shopLists: object[];
 	shopArchivedLists: object[];
+	temporaryList: string[];
+	element: string;
 }
 
 const initialState = {
 	shopLists: [],
 	shopArchivedLists: [],
+	addNew: false,
+	temporaryList: []
 };
 
 export const purchases = (state = initialState, action: Actions): object => {
 	switch (action.type) {
+		case 'ADD_NEW':
+			return {
+				...state,
+				addNew: action.addNew,
+			};
+		case 'SET_TEMPORARY_LIST':
+			return {
+				...state,
+				temporaryList: [...state.temporaryList, action.element],
+			};
 		case 'SET_LIST':
 			return {
 				...state,
@@ -20,7 +35,7 @@ export const purchases = (state = initialState, action: Actions): object => {
 		case 'SET_ARCHIVED_LIST':
 			return {
 				...state,
-				shopLists: action.shopLists,
+				shopArchivedLists: action.shopArchivedLists,
 			};
 		default:
 			return state;
